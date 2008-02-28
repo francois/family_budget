e03fc83701c2796b7b7528a56f1528588b39c43b
François Beausoleil <francois@teksol.info>
2008-02-28 13:55:32 -0500

    Corrected validates_presence_of :debit_account_id in Transfer.

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -2,5 +2,5 @@ class Transfer < ActiveRecord::Base
   belongs_to :family
   belongs_to :debit_account, :class_name => "Account"
   belongs_to :credit_account, :class_name => "Account"
-  validates_presence_of :family_id, :debit_accoun_id, :credit_account_id, :posted_on
+  validates_presence_of :family_id, :debit_account_id, :credit_account_id, :posted_on
 end
