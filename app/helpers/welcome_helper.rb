7c2da594b980cff50749bdc67162fbc8ce72f481
François Beausoleil <francois@teksol.info>
2008-02-28 11:19:27 -0500

    Updated WelcomeController#index view now that Transfer has debit_account, debit_account.
    Replaced From with Debit, With with Credit.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,9 +1,9 @@
 module WelcomeHelper
   def mini_form(object_name, options={})
-    options.reverse_merge!(:from_label => "Quoi", :with_label => "Payé avec", :amount_label => "Montant")
+    options.reverse_merge!(:debit_label => "Quoi", :credit_label => "Payé avec", :amount_label => "Montant")
     object = instance_variable_get("@#{object_name}")
-    from, with = options[:from], options[:with]
-    from_label, with_label, amount_label = options[:from_label], options[:with_label], options[:amount_label]
-    render :partial => "welcome/form", :locals => {:object => object, :object_name => object_name, :from => from, :with => with, :from_label => from_label, :with_label => with_label, :amount_label => amount_label}
+    debit_accounts, credit_accounts = options[:debit_accounts], options[:credit_accounts]
+    debit_label, credit_label, amount_label = options[:debit_label], options[:credit_label], options[:amount_label]
+    render :partial => "welcome/form", :locals => {:object => object, :object_name => object_name, :debit_accounts => debit_accounts, :credit_accounts => credit_accounts, :debit_label => debit_label, :credit_label => credit_label, :amount_label => amount_label}
   end
 end
