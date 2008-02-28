7ddeebf06d3d5ac7e9f576cb8ddb55c9a09bd300
François Beausoleil <francois@teksol.info>
2008-02-28 11:12:05 -0500

    New Family#budget_for.
    Budget now links and validates it's stuff.

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,2 +1,8 @@
 class Budget < ActiveRecord::Base
+  belongs_to :family
+  belongs_to :account
+  validates_presence_of :family_id, :account_id, :year, :month
+  validates_numericality_of :year, :month, :amount
+
+  attr_accessible :family, :account, :year, :month, :amount
 end
--- app/models/family.rb
+++ app/models/family.rb
@@ -3,4 +3,14 @@ class Family < ActiveRecord::Base
   has_many :transfers
   has_many :accounts, :order => "name", :extend => Extensions::Accounts
   has_many :budgets, :order => "account_id, year, month", :extend => Extensions::Budgets
+
+  def budget_for(year, month)
+    returning({}) do |hash|
+      self.budgets.for_period(year, month).each {|budget| hash[budget.account] = budget}
+      budgeted_accounts = hash.keys
+      budgetable_accounts = self.accounts.expenses + self.accounts.incomes
+      budgetable_accounts.delete_if {|a| budgeted_accounts.include?(a)}
+      budgetable_accounts.each {|account| hash[account] = self.budgets.build(:account => account, :year => year, :month => month, :amount => 0)}
+    end.values
+  end
 end
