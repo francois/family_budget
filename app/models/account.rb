a48618fc806b15d0dcf368a46047a62770dc3503
François Beausoleil <francois@teksol.info>
2008-02-28 14:31:16 -0500

    Account grew #real_amount_in_period and #budget_amount_in_period.

--- app/models/account.rb
+++ app/models/account.rb
@@ -11,7 +11,26 @@ class Account < ActiveRecord::Base
   before_validation {|a| a.purpose = a.purpose.downcase unless a.purpose.blank?}
   validates_inclusion_of :purpose, :in => ValidPurposes
 
-  has_many :budgets, :order => "year, month"
+  has_many :budgets, :order => "year, month", :extend => Extensions::Budgets
+
+  def real_amount_in_period(year, month)
+    debits = self.family.transfers.all_debits_by_account_year_month(self, year, month)
+    credits = self.family.transfers.all_credits_by_account_year_month(self, year, month)
+
+    debit_amount = debits.map(&:amount).compact.sum
+    credit_amount = credits.map(&:amount).compact.sum
+
+    case purpose
+    when "asset", "expense"
+      debit_amount - credit_amount
+    when "liability", "equity", "income"
+      credit_amount - debit_amount
+    end
+  end
+
+  def budget_amount_in_period(year, month)
+    self.budgets.for_account_year_month(self, year, month).amount
+  end
 
   def to_s
     name
