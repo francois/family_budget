a2deda6e24fa8ffcfa608c3817dfb461e4984079
François Beausoleil <francois@teksol.info>
2008-02-28 12:40:37 -0500

    WelcomesHelper#mini_form didn't have the right partial name.

--- app/helpers/welcomes_helper.rb
+++ app/helpers/welcomes_helper.rb
@@ -1,9 +1,9 @@
-module WelcomeHelper
+module WelcomesHelper
   def mini_form(object_name, options={})
     options.reverse_merge!(:debit_label => "Quoi", :credit_label => "Payé avec", :amount_label => "Montant")
     object = instance_variable_get("@#{object_name}")
     debit_accounts, credit_accounts = options[:debit_accounts], options[:credit_accounts]
     debit_label, credit_label, amount_label = options[:debit_label], options[:credit_label], options[:amount_label]
-    render :partial => "welcome/form", :locals => {:object => object, :object_name => object_name, :debit_accounts => debit_accounts, :credit_accounts => credit_accounts, :debit_label => debit_label, :credit_label => credit_label, :amount_label => amount_label}
+    render :partial => "welcomes/form", :locals => {:object => object, :object_name => object_name, :debit_accounts => debit_accounts, :credit_accounts => credit_accounts, :debit_label => debit_label, :credit_label => credit_label, :amount_label => amount_label}
   end
 end
