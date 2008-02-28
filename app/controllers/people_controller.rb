dd382c012f8a3d8030b0be60d1f3f3ad95248eaf
François Beausoleil <francois@teksol.info>
2008-02-28 15:42:06 -0500

    Frenchified and normalized login/signup pages.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,19 +2,24 @@ class PeopleController < ApplicationController
   skip_before_filter :login_required, :only => %w(new create)
 
   def new
+    @family = Family.new
+    @person = Person.new
     render
   end
 
   def create
     cookies.delete :auth_token
     reset_session
-    @person = Person.new(params[:person])
-    if @person.save then
-      self.current_person = @person
-      redirect_back_or_default("/")
-      flash[:notice] = "Thanks for signing up!"
-    else
-      render :action => "new"
+    Family.transaction do
+      @family = Family.create!(params[:family])
+      @person = @family.people.build(params[:person])
+      if @person.save then
+        self.current_person = @person
+        redirect_back_or_default("/")
+        flash[:notice] = "Thanks for signing up!"
+      else
+        render :action => "new"
+      end
     end
   end
 end
