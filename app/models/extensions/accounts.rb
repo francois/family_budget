d43046f4d160f13686165491e913471a0b121523
François Beausoleil <francois@teksol.info>
2008-02-28 22:25:21 -0500

    Order the accounts consistently on the AccountsController#index page.

--- app/models/extensions/accounts.rb
+++ app/models/extensions/accounts.rb
@@ -1,17 +1,25 @@
 module Extensions
   module Accounts
+    def all_by_type_and_name
+      find(:all, :order => "purpose, name")
+    end
+
     def assets
       find(:all, :conditions => {:purpose => Account::Asset})
     end
+
     def liabilities
       find(:all, :conditions => {:purpose => Account::Liability})
     end
+
     def equities
       find(:all, :conditions => {:purpose => Account::Equity})
     end
+
     def incomes
       find(:all, :conditions => {:purpose => Account::Income})
     end
+
     def expenses
       find(:all, :conditions => {:purpose => Account::Expense})
     end
