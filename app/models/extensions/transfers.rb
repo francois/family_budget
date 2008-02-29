6c76bf5850fd41ccf5447d7ae7a34b15ceb558ec
François Beausoleil <francois@teksol.info>
2008-02-28 23:01:58 -0500

    Extension::Transfers don't crash anymore when in January or December.

--- app/models/extensions/transfers.rb
+++ app/models/extensions/transfers.rb
@@ -1,11 +1,15 @@
 module Extensions
   module Transfers
     def all_debits_by_account_year_month(account, year, month)
-      find(:all, :conditions => ["debit_account_id = ? AND posted_on BETWEEN ? AND ?", account.id, Date.new(year, month, 1), Date.new(year, month + 1, 1) - 1], :order => "posted_on")
+      this_month = Date.new(year, month, 1)
+      next_month = this_month >> 1
+      find(:all, :conditions => ["debit_account_id = ? AND posted_on >= ? AND posted_on <= ?", account.id, this_month, next_month], :order => "posted_on")
     end
 
     def all_credits_by_account_year_month(account, year, month)
-      find(:all, :conditions => ["credit_account_id = ? AND posted_on BETWEEN ? AND ?", account.id, Date.new(year, month, 1), Date.new(year, month + 1, 1) - 1], :order => "posted_on")
+      this_month = Date.new(year, month, 1)
+      next_month = this_month >> 1
+      find(:all, :conditions => ["credit_account_id = ? AND posted_on >= ? AND posted_on <= ?", account.id, this_month, next_month], :order => "posted_on")
     end
   end
 end
