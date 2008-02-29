3506e6ee719cf264e2c050e127baa2472644f1d0
François Beausoleil <francois@teksol.info>
2008-02-28 23:08:30 -0500

    BudgetsHelper#month_name was moved to ApplicationHelper in a previous commit.

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -1,6 +1,2 @@
 module BudgetsHelper
-  MONTH_NAMES = %w(Janvier Février Mars Avril Mai Juin Juillet Août September Octobre Novembre Décembre)
-  def month_name(month)
-    MONTH_NAMES[month-1]
-  end
 end
