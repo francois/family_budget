3110cc5f7b208b2e0ecc463d6eb0fe2c35954b83
François Beausoleil <francois@teksol.info>
2008-11-02 22:55:04 -0500

    Begin testing imports.

---
+++ app/models/import.rb
@@ -0,0 +1,27 @@
+require "generator"
+
+class Import
+  attr_accessor :family, :data
+
+  def initialize(params={})
+    params.each_pair do |attr, value|
+      send("#{attr}=", value) if respond_to?("#{attr}=")
+    end
+  end
+
+  def create_bank_accounts!
+    assert_family_present
+
+    bankids = @data.scan(/<BANKID>(.+(?=<|$))/)
+    acctids = @data.scan(/<ACCTID>(.+(?=<|$))/)
+    SyncEnumerator.new(bankids.first, acctids.first).each do |bankid, acctid|
+      bank_account = family.bank_accounts.find_or_initialize_by_bank_number_and_account_number(bankid, acctid)
+      bank_account.save! if bank_account.new_record?
+    end
+  end
+
+  private
+  def assert_family_present
+    raise ArgumentError, "No family assigned to #{inspect}" unless family
+  end
+end
---
+++ test/unit/import_test.rb
@@ -0,0 +1,206 @@
+require File.dirname(__FILE__) + "/../test_helper"
+
+class ImportTest < Test::Unit::TestCase
+  context "An import" do
+    context "referring to a new bank and account" do
+      setup do
+        @import = Import.new(:data => qfx(:bankid => "1221", :acctid => "77321812"), :family => families(:beausoleil))
+      end
+
+      context "calling #create_bank_accounts!" do
+        setup do
+          @import.create_bank_accounts!
+        end
+
+        should_change "BankAccount.count", :by => 1
+      end
+    end
+
+    context "referring to an existing bank and account" do
+      setup do
+        @import = Import.new(:data => qfx, :family => families(:beausoleil))
+      end
+
+      context "calling #create_bank_accounts!" do
+        setup do
+          @import.create_bank_accounts!
+        end
+
+        should_not_change "BankAccount.count"
+      end
+    end
+  end
+
+  context "A QFX data file example" do
+    context "with default options" do
+      setup do
+        @qfx = qfx
+      end
+
+      should "NOT be blank?" do
+        assert !@qfx.blank?
+      end
+
+      should "contain <BANKID>0912039" do
+        assert_include "<BANKID>#{bank_accounts(:checking).bank_number}", @qfx
+      end
+
+      should "contain <ACCTID>928192828" do
+        assert_include "<ACCTID>#{bank_accounts(:checking).account_number}", @qfx
+      end
+
+      should "contain <ACCTTYPE>CHECKING" do
+        assert_include "<ACCTTYPE>CHECKING", @qfx
+      end
+
+      should "contain <DTSTART>[today]" do
+        assert_include "<DTSTART>#{Date.today.to_time.strftime("%Y%m%d%H%M%S")}", @qfx
+      end
+
+      should "contain <DTEND>[today]" do
+        assert_include "<DTEND>#{Date.today.to_time.strftime("%Y%m%d%H%M%S")}", @qfx
+      end
+
+      should "contain <DTPOSTED>[today]" do
+        assert_include "<DTPOSTED>#{Date.today.to_time.strftime("%Y%m%d%H%M%S")}", @qfx
+      end
+
+      should "contain <TRNAMT>-291.99" do
+        assert_include "<TRNAMT>-291.99", @qfx
+      end
+
+      should "contain <NAME>ACCOUNT TRANSFER" do
+        assert_include "<NAME>ACCOUNT TRANSFER", @qfx
+      end
+
+      should "contain <MEMO>W3 TRANSFER - 9382\\s" do
+        assert_include "<MEMO>W3 TRANSFER - 9382 ", @qfx
+      end
+    end
+
+    context "with bankid option" do
+      setup do
+        @qfx = qfx(:bankid => "123999")
+      end
+
+      should "contain <BANKID>123999" do
+        assert_include "<BANKID>123999", @qfx
+      end
+    end
+
+    context "with acctid option" do
+      setup do
+        @qfx = qfx(:acctid => "999888")
+      end
+
+      should "contain <ACCTID>999888" do
+        assert_include "<ACCTID>999888", @qfx
+      end
+    end
+
+    context "with accttype option" do
+      setup do
+        @qfx = qfx(:accttype => "SAVINGS")
+      end
+
+      should "contain <ACCTTYPE>SAVINGS" do
+        assert_include "<ACCTTYPE>SAVINGS", @qfx
+      end
+    end
+
+    context "with dtstart option" do
+      setup do
+        @qfx = qfx(:dtstart => Date.new(2008, 10, 31))
+      end
+
+      should "contain <DTSTART>20081031000000" do
+        assert_include "<DTSTART>20081031000000", @qfx
+      end
+    end
+
+    context "with dtend option" do
+      setup do
+        @qfx = qfx(:dtend => Date.new(2008, 11, 2))
+      end
+
+      should "contain <DTEND>20081102000000" do
+        assert_include "<DTEND>20081102000000", @qfx
+      end
+    end
+
+    context "with dtposted option" do
+      setup do
+        @qfx = qfx(:dtposted => Date.new(2008, 11, 3))
+      end
+
+      should "contain <DTPOSTED>20081103000000" do
+        assert_include "<DTPOSTED>20081103000000", @qfx
+      end
+    end
+
+    context "with trnamt option" do
+      setup do
+        @qfx = qfx(:trnamt => "-1312.32")
+      end
+
+      should "contain <TRNAMT>-1312.32" do
+        assert_include "<TRNAMT>-1312.32", @qfx
+      end
+    end
+
+    context "with fitid option" do
+      setup do
+        @qfx = qfx(:fitid => "555777888999566")
+      end
+
+      should "contain <FITID>555777888999566" do
+        assert_include "<FITID>555777888999566", @qfx
+      end
+    end
+
+    context "with name option" do
+      setup do
+        @qfx = qfx(:name => "INTER-ACCOUNT TRANSFER")
+      end
+
+      should "contain <NAME>INTER-ACCOUNT TRANSFER" do
+        assert_include "<NAME>INTER-ACCOUNT TRANSFER", @qfx
+      end
+    end
+
+    context "with memo option" do
+      setup do
+        @qfx = qfx(:memo => "PAYPAL LTD")
+      end
+
+      should "contain <MEMO>PAYPAL LTD" do
+        assert_include "<MEMO>PAYPAL LTD", @qfx
+      end
+    end
+  end
+
+  protected
+  def qfx(options={})
+    options.stringify_keys!
+    options.reverse_merge!(
+      "acctid"   => bank_accounts(:checking).account_number,
+      "bankid"   => bank_accounts(:checking).bank_number, 
+      "accttype" => "CHECKING",
+      "dtstart"  => Date.today,
+      "dtend"    => Date.today,
+      "dtposted" => Date.today,
+      "trnamt"   => "-291.99",
+      "name"     => "ACCOUNT TRANSFER",
+      "memo"     => "W3 TRANSFER - 9382 ")
+    options.each_pair do |key, value|
+      next unless key.starts_with?("dt")
+      options[key] = value.to_time.strftime("%Y%m%d%H%M%S")
+    end
+
+    returning(File.read(File.join(Rails.root, "test", "fixtures", "single_transaction.qfx"))) do |data|
+      %w(bankid acctid accttype dtposted dtstart dtend fitid trnamt name memo).each do |field|
+        data.gsub!("__#{field.upcase}__", options[field]) if options.has_key?(field)
+      end
+    end
+  end
+end
