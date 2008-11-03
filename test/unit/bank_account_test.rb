18f95e90101f4e67b0cfe8b5aac0e2db9250d3a3
François Beausoleil <francois@teksol.info>
2008-11-02 22:53:34 -0500

    Relaxed bank account validation to allow account_id to be nil.
    While importing transactions, we create new bank accounts as required, but we won't yet know during import which account to use.  We need a human touch here!

--- test/unit/bank_account_test.rb
+++ test/unit/bank_account_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class BankAccountTest < Test::Unit::TestCase
   should_have_valid_fixtures
-  should_require_attributes :family_id, :account_id, :bank_number, :account_number
+  should_require_attributes :family_id, :bank_number, :account_number
   should_protect_attributes :family_id, :account_id
   should_allow_attributes :family, :account, :bank_number, :account_number
   should_belong_to :account, :family
