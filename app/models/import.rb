9151582e7c8d4b2ac127c50427f657653f6a26c3
François Beausoleil <francois@teksol.info>
2008-11-08 08:45:46 -0500

    Implemented bank account and transaction importing.

--- app/models/import.rb
+++ app/models/import.rb
@@ -10,22 +10,33 @@ class Import
   end
 
   def create_bank_accounts!
-    assert_family_present
-
-    bankids = @data.scan(/<BANKID>(.+(?=<|$))/)
-    acctids = @data.scan(/<ACCTID>(.+(?=<|$))/)
-    SyncEnumerator.new(bankids.first, acctids.first).each do |bankid, acctid|
-      bank_account = family.bank_accounts.find_or_initialize_by_bank_number_and_account_number(bankid, acctid)
+    accounts.each do |account|
+      bank_account = bank_account_of(account)
       bank_account.save! if bank_account.new_record?
     end
   end
 
   def create_transactions!
-    
+    accounts.each do |account|
+      bank_account = bank_account_of(account)
+      account.transactions.each do |txn|
+        transaction = family.transactions.find_or_initialize_by_fitid(txn.number)
+        transaction.update_attributes!(:bank_account => bank_account, :amount => txn.amount, :name => txn.name, :memo => txn.memo, :posted_on => txn.timestamp) if transaction.new_record?
+      end
+    end
   end
 
   private
   def assert_family_present
     raise ArgumentError, "No family assigned to #{inspect}" unless family
+  end
+
+  def bank_account_of(account)
+    family.bank_accounts.find_or_create_by_bank_number_and_account_number(account.bank_id, account.number)
+  end
+
+  def accounts
+    assert_family_present
+    @accounts ||= QuickenParser.parse(data)
   end
 end
