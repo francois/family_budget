6e97e4a2204537cda6d25ff9c2d96fbd312102cc
François Beausoleil <francois@teksol.info>
2008-11-08 22:59:06 -0500

    Made form multipart

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -3,6 +3,9 @@ class ImportsController < ApplicationController
   end
 
   def create
+    @import = Import.new(params[:import])
+    @import.family = current_family
+    @import.process!
     redirect_to transactions_url
   end
 end
