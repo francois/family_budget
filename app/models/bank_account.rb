19eb473e8bda1439d90acb839e1766a76b055676
François Beausoleil <francois@teksol.info>
2008-11-08 22:49:22 -0500

    Don't require presence of bank number in BankAccount.
    Royal Bank doesn't send the bank number for credit cards.

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount < ActiveRecord::Base
   belongs_to :account
   belongs_to :family
-  validates_presence_of :family_id, :bank_number, :account_number
+  validates_presence_of :family_id, :account_number
   attr_accessible :family, :account, :bank_number, :account_number
   has_many :transactions
 
