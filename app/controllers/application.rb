3b5628b4a9bb4ea54e615c728518e44b393eb3fd
François Beausoleil <francois@teksol.info>
2008-11-20 15:34:40 -0500

    Fixed functional test failures because #current_date was missing.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,12 +4,24 @@ class ApplicationController < ActionController::Base
   include ExceptionNotifiable
 
   helper :all
-  helper_method :current_family
+  helper_method :current_family, :current_date
 
   before_filter :login_required
 
   protected
   def current_family
     @_current_family ||= self.current_person.family if logged_in?
+  end
+
+  def current_date
+    current_time.to_date
+  end
+
+  def current_time
+    timezone.utc_to_local(Time.now.utc)
+  end
+
+  def timezone
+    TZInfo::Timezone.get("America/Montreal")
   end
 end
