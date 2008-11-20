5611eec04f276c3812a66cb79b8d8567f11fa61f
François Beausoleil <francois@teksol.info>
2008-11-20 11:15:31 -0500

    Freshened dependencies, load test dependencies during test running

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,11 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 
+gem "francois-shoulda"
+require "shoulda/rails"
+gem "mocha"
+require "mocha"
+
 class Test::Unit::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
   # in a transaction that's rolled back on completion.  This ensures that the
