b78b0b6fc2fb21e7dec847da97153f56af427ae9
François Beausoleil <francois@teksol.info>
2008-11-21 10:48:31 -0500

    Implemented filtering of bank transactions in the pending list

--- app/controllers/bank_transactions_controller.rb
+++ app/controllers/bank_transactions_controller.rb
@@ -1,7 +1,13 @@
 class BankTransactionsController < ApplicationController
-  helper_method :bank_transactions, :bank_transaction, :accounts, :income_and_expense_accounts
+  helper_method :bank_transactions, :bank_transaction, :accounts, :income_and_expense_accounts, :bank_accounts
 
   def index
+    @period = params[:period]
+    @bank_account_id = params[:bank_account_id]
+    root = current_family.bank_transactions.pending.by_posted_on
+    root = root.in_period(params[:period]) unless params[:period].blank?
+    root = root.on_bank_account(current_family.bank_accounts.find(params[:bank_account_id])) unless params[:bank_account_id].blank?
+    @bank_transactions = root.paginate(:per_page => 200, :page => params[:page])
   end
 
   def create
@@ -22,5 +28,9 @@ class BankTransactionsController < ApplicationController
 
   def income_and_expense_accounts
     @accounts ||= current_family.accounts.purposes(Account::Income, Account::Expense).by_type_and_name
+  end
+
+  def bank_accounts
+    @bank_accounts ||= current_family.bank_accounts
   end
 end
