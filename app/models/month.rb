34f16275788cd0cea44c41e68e350623d78d59af
François Beausoleil <francois@teksol.info>
2009-01-11 13:19:35 -0500

    Implemented Month
    Month holds all budget accounts for a period

---
+++ app/models/month.rb
@@ -0,0 +1,25 @@
+class Month
+  attr_accessor :family, :year, :month
+
+  def initialize(params={})
+    self.attributes = params
+  end
+
+  def attributes=(params)
+    params.each do |key, val|
+      send("#{key}=", val)
+    end
+  end
+
+  def date=(date)
+    @year, @month = date.year, date.month
+  end
+
+  def valid?
+    !(@family.blank? || [@year, @month].all?(&:blank?))
+  end
+
+  def errors
+    @errors ||= ActiveRecord::Errors.new(self)
+  end
+end
---
+++ test/unit/month_test.rb
@@ -0,0 +1,33 @@
+require File.dirname(__FILE__) + "/../test_helper"
+
+class MonthTest < Test::Unit::TestCase
+  context "A month" do
+    setup do
+      @month = Month.new
+    end
+
+    context "without a family or date or year/month" do
+      should "be invalid" do
+        assert !@month.valid?
+      end
+    end
+
+    context "with a family and date" do
+      setup do
+        @month.attributes = {:family => families(:beausoleil), :date => Date.today}
+      end
+
+      should "be valid" do
+        assert_valid @month
+      end
+
+      should "have a year of Today" do
+        assert_equal Date.today.year, @month.year
+      end
+
+      should "have a month of Today" do
+        assert_equal Date.today.month, @month.month
+      end
+    end
+  end
+end
