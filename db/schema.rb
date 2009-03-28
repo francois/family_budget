59af69d77982c1a16e83791d2ea0b924c0963135
François Beausoleil <francois@teksol.info>
2009-03-27 21:11:11 -0400

    Updated to Rails 2.3

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 ActiveRecord::Schema.define(:version => 20081218201104) do
 
   create_table "accounts", :force => true do |t|
-    t.integer  "family_id",  :limit => 11
+    t.integer  "family_id",  :limit => 8
     t.string   "name"
     t.string   "purpose"
     t.datetime "created_at"
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(:version => 20081218201104) do
   end
 
   create_table "bank_accounts", :force => true do |t|
-    t.integer  "family_id",              :limit => 11
+    t.integer  "family_id",              :limit => 8
     t.string   "bank_number"
     t.string   "display_account_number"
-    t.integer  "account_id",             :limit => 11
+    t.integer  "account_id",             :limit => 8
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "salted_account_number"
@@ -32,16 +32,16 @@ ActiveRecord::Schema.define(:version => 20081218201104) do
   add_index "bank_accounts", ["family_id"], :name => "by_family"
 
   create_table "bank_transactions", :force => true do |t|
-    t.integer  "family_id",       :limit => 11
-    t.integer  "bank_account_id", :limit => 11
+    t.integer  "family_id",       :limit => 8
+    t.integer  "bank_account_id", :limit => 8
     t.date     "posted_on"
-    t.decimal  "amount",                        :precision => 12, :scale => 2
+    t.decimal  "amount",                       :precision => 12, :scale => 2
     t.string   "name"
     t.string   "memo"
     t.string   "fitid"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "transfer_id",     :limit => 11
+    t.integer  "transfer_id",     :limit => 8
     t.datetime "ignored_at"
   end
 
@@ -49,17 +49,17 @@ ActiveRecord::Schema.define(:version => 20081218201104) do
   add_index "bank_transactions", ["family_id", "posted_on"], :name => "by_family_posted"
 
   create_table "bank_transactions_transfers", :id => false, :force => true do |t|
-    t.integer "bank_transaction_id", :limit => 11
-    t.integer "transfer_id",         :limit => 11
+    t.integer "bank_transaction_id", :limit => 8
+    t.integer "transfer_id",         :limit => 8
   end
 
   add_index "bank_transactions_transfers", ["bank_transaction_id", "transfer_id"], :name => "by_bank_transaction_transfer", :unique => true
   add_index "bank_transactions_transfers", ["transfer_id", "bank_transaction_id"], :name => "by_transfer_bank_transaction", :unique => true
 
   create_table "budgets", :force => true do |t|
-    t.integer  "family_id",   :limit => 11
-    t.integer  "account_id",  :limit => 11
-    t.decimal  "amount",                    :precision => 9, :scale => 2, :default => 0.0
+    t.integer  "family_id",   :limit => 8
+    t.integer  "account_id",  :limit => 8
+    t.decimal  "amount",                   :precision => 9, :scale => 2, :default => 0.0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "starting_on"
@@ -81,19 +81,19 @@ ActiveRecord::Schema.define(:version => 20081218201104) do
     t.datetime "updated_at"
     t.string   "remember_token"
     t.datetime "remember_token_expires_at"
-    t.integer  "family_id",                 :limit => 11
+    t.integer  "family_id",                 :limit => 8
     t.boolean  "admin"
   end
 
   create_table "transfers", :force => true do |t|
-    t.integer  "family_id",         :limit => 11
+    t.integer  "family_id",         :limit => 8
     t.date     "posted_on"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "debit_account_id",  :limit => 11
-    t.integer  "credit_account_id", :limit => 11
-    t.decimal  "amount",                          :precision => 9, :scale => 2
+    t.integer  "debit_account_id",  :limit => 8
+    t.integer  "credit_account_id", :limit => 8
+    t.decimal  "amount",                         :precision => 9, :scale => 2
   end
 
 end
