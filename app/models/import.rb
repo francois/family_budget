08a30b86a6fcbf2cfb5d393d6d31fa178ac99925
François Beausoleil <francois@teksol.info>
2009-05-02 17:01:20 -0400

    Fixed problem with passing Money objects around to ActiveRecord objects when they expected a DECIMAL value (BigDecimal or equivalent).

--- app/models/import.rb
+++ app/models/import.rb
@@ -19,7 +19,7 @@ class Import
       raise "Could not find account with #{account.inspect}" if bank_account.nil?
       account.transactions.each do |txn|
         bank_transaction = family.bank_transactions.find_or_initialize_by_fitid(txn.number)
-        bank_transaction.update_attributes!(:bank_account => bank_account, :amount => txn.amount, :name => txn.name, :memo => txn.memo, :posted_on => txn.timestamp) if bank_transaction.new_record?
+        bank_transaction.update_attributes!(:bank_account => bank_account, :amount => txn.amount.to_s, :name => txn.name, :memo => txn.memo, :posted_on => txn.timestamp) if bank_transaction.new_record?
       end
     end
     
--- test/unit/import_test.rb
+++ test/unit/import_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../test_helper"
+require 'test_helper'
 
 class ImportTest < ActiveSupport::TestCase
   context "An import" do
