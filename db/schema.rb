7bc04479dce050be5c542275386dd973a22c6ad8
François Beausoleil <francois@teksol.info>
2009-05-02 16:41:08 -0400

    Switched from Rails 2.1.0 to Rails 2.3.2

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 ActiveRecord::Schema.define(:version => 20081218201104) do
 
   create_table "accounts", :force => true do |t|
-    t.integer  "family_id",  :limit => 11
+    t.integer  "family_id"
     t.string   "name"
     t.string   "purpose"
     t.datetime "created_at"
@@ -32,14 +32,10 @@ ActiveRecord::Schema.define(:version => 20081218201104) do
   add_index "bank_accounts", ["family_id"], :name => "by_family"
 
   create_table "bank_transactions", :force => true do |t|
-    t.integer  "family_id",       :limit => 11
-    t.integer  "bank_account_id", :limit => 11
+    t.integer  "family_id"
+    t.integer  "bank_account_id"
     t.date     "posted_on"
-<<<<<<< HEAD:db/schema.rb
-    t.decimal  "amount",                        :precision => 12, :scale => 2
-=======
-    t.decimal  "amount",          :precision => 12, :scale => 2
->>>>>>> 925db16... Migrated development and test databases to PostgreSQL:db/schema.rb
+    t.decimal  "amount"
     t.string   "name"
     t.string   "memo"
     t.string   "fitid"
@@ -53,8 +49,8 @@ ActiveRecord::Schema.define(:version => 20081218201104) do
   add_index "bank_transactions", ["family_id", "posted_on"], :name => "by_family_posted"
 
   create_table "bank_transactions_transfers", :id => false, :force => true do |t|
-    t.integer "bank_transaction_id", :limit => 11
-    t.integer "transfer_id",         :limit => 11
+    t.integer "bank_transaction_id"
+    t.integer "transfer_id"
   end
 
   add_index "bank_transactions_transfers", ["bank_transaction_id", "transfer_id"], :name => "by_bank_transaction_transfer", :unique => true
@@ -63,7 +59,7 @@ ActiveRecord::Schema.define(:version => 20081218201104) do
   create_table "budgets", :force => true do |t|
     t.integer  "family_id"
     t.integer  "account_id"
-    t.decimal  "amount",      :precision => 9, :scale => 2, :default => 0.0
+    t.decimal  "amount",      :default => 0.0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "starting_on"
@@ -85,25 +81,19 @@ ActiveRecord::Schema.define(:version => 20081218201104) do
     t.datetime "updated_at"
     t.string   "remember_token"
     t.datetime "remember_token_expires_at"
-    t.integer  "family_id",                 :limit => 11
+    t.integer  "family_id"
     t.boolean  "admin"
   end
 
   create_table "transfers", :force => true do |t|
-    t.integer  "family_id",         :limit => 11
+    t.integer  "family_id"
     t.date     "posted_on"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-<<<<<<< HEAD:db/schema.rb
-    t.integer  "debit_account_id",  :limit => 11
-    t.integer  "credit_account_id", :limit => 11
-    t.decimal  "amount",                          :precision => 9, :scale => 2
-=======
     t.integer  "debit_account_id"
     t.integer  "credit_account_id"
-    t.decimal  "amount",            :precision => 9, :scale => 2
->>>>>>> 925db16... Migrated development and test databases to PostgreSQL:db/schema.rb
+    t.decimal  "amount"
   end
 
 end
