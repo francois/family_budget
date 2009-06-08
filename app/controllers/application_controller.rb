15135669a17628a1100b12025b87912b430f3fbf
François Beausoleil <francois@teksol.info>
2009-06-07 21:29:32 -0400

    Moved helpers to enable reuse from ExtJS dashboard.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   helper :all
   helper_method :current_family, :current_date
+  helper_method :current_period, :most_active_expense_accounts, :most_active_income_accounts, :total_incomes_amount, :total_expenses_amount, :total_expenses_per_period, :total_incomes_per_period, :dates_for_period
 
   before_filter :login_required
 
@@ -23,5 +24,47 @@ class ApplicationController < ActionController::Base
 
   def timezone
     TZInfo::Timezone.get("America/New_York")
+  end
+
+  def current_period
+    @current_period ||= if params[:period].blank? then
+                          current_date.beginning_of_month.to_date
+                        else
+                          params[:period].gsub(/\D/, "")
+                        end
+  end
+
+  def most_active_expense_accounts
+    @most_active_expense_accounts ||= current_family.accounts.most_active_expense_in_period(current_period).paginate(:per_page => 10, :page => 1)
+  end
+
+  def total_expenses_amount
+    @total_expense_amount ||= most_active_expense_accounts.map(&:amount).map(&:to_f).sum
+  end
+
+  def most_active_income_accounts
+    @most_active_income_accounts ||= current_family.accounts.most_active_income_in_period(current_period).paginate(:per_page => 10, :page => 1)
+  end
+
+  def total_incomes_amount
+    @total_income_amount ||= most_active_income_accounts.map(&:amount).map(&:to_f).sum
+  end
+
+  def total_expenses_per_period(count=12)
+    @total_expenses_per_period ||= current_family.expense_amounts_in_dates(dates_for_period).map(&:last)
+  end
+
+  def total_incomes_per_period(count=12)
+    @total_incomes_per_period ||= current_family.income_amounts_in_dates(dates_for_period).map(&:last)
+  end
+
+  def dates_for_period(count=12)
+    start, stop = (current_date << count - 1), (current_date >> 1).at_beginning_of_month.to_date
+    returning([]) do |dates|
+      while start < stop
+        dates << start.at_beginning_of_month.to_date
+        start = start >> 1
+      end
+    end
   end
 end
