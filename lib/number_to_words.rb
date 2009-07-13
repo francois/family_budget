f04346542557cc8d091db82b6d6d6e4b4650169b
François Beausoleil <francois@teksol.info>
2009-07-13 00:17:16 -0400

    Convert integers within the [-9999, 9999] range.

--- lib/number_to_words.rb
+++ lib/number_to_words.rb
@@ -1,7 +1,37 @@
 module NumberToWords
   extend self
 
-  def number_to_words(number)
-    "zero"
+  SINGLES  = %w(zero one two three four five six seven eight nine)
+  TEENS    = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+  TENS     = %w(twenty thirty fourty fifty sixty seventy eighty ninety)
+  HUNDREDS = %w(hundred thousand)
+
+  def number_to_words(number, zero_as_text=true)
+    num    = number.abs
+    result = case num
+             when 1000..9999
+               SINGLES[num / 1000] + " " + HUNDREDS[1] + " " + number_to_words(num % 1000, false)
+             when 100..999
+               SINGLES[num / 100] + " " + HUNDREDS[0] + " " + number_to_words(num % 100, false)
+             when 20..99
+               TENS[(num / 10) - 2] + " " + number_to_words(num % 10, false)
+             when 10..19
+               TEENS[num - 10]
+             when 0..9
+               if num.zero? then
+                 if zero_as_text then
+                   "zero"
+                 else
+                   ""
+                 end
+               else
+                 SINGLES[num]
+               end
+             else
+               raise OutOfBoundsException, "This converter can only convert 0 to 99999, negative or positive.  Received #{number}"
+             end.strip
+    number < 0 ? "minus #{result}" : result
   end
+
+  class OutOfBoundsException < ArgumentError; end
 end
--- test/unit/number_to_words_test.rb
+++ test/unit/number_to_words_test.rb
@@ -1,11 +1,26 @@
 require "test_helper"
 
 class NumberToWordsTest < ActiveSupport::TestCase
-  def self.should_convert(number, words)
+  {
+       0 => "zero",          1 => "one",               2 => "two",                      3 => "three",         4 => "four",
+       5 => "five",          6 => "six",               7 => "seven",                    8 => "eight",         9 => "nine",
+      10 => "ten",          11 => "eleven",           12 => "twelve",                  13 => "thirteen",     14 => "fourteen",
+      15 => "fifteen",      16 => "sixteen",          17 => "seventeen",               18 => "eighteen",     19 => "nineteen",
+      20 => "twenty",       21 => "twenty one",       22 => "twenty two",              33 => "thirty three", 44 => "fourty four",
+      55 => "fifty five",   66 => "sixty six",        77 => "seventy seven",           88 => "eighty eight", 99 => "ninety nine",
+     100 => "one hundred", 101 => "one hundred one", 122 => "one hundred twenty two", 999 => "nine hundred ninety nine",
+     309 => "three hundred nine",
+     563 => "five hundred sixty three",
+     708 => "seven hundred eight",
+      -1 => "minus one",
+     -23 => "minus twenty three",
+    -356 => "minus three hundred fifty six",
+    1000 => "one thousand",
+    1234 => "one thousand two hundred thirty four",
+    9999 => "nine thousand nine hundred ninety nine",
+  }.each do |number, words|
     should "convert #{number} to #{words.inspect}" do
       assert_equal words, NumberToWords.number_to_words(number)
     end
   end
-
-  should_convert 0, "zero"
 end
